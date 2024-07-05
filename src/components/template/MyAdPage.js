81ab0badb282cd704da901c8b26bf9b92ae49bd0
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-07-05 13:19:26 +0330

    fle added

--- src/components/template/MyAdPage.js
+++ src/components/template/MyAdPage.js
@@ -7,7 +7,10 @@ function MyAdPage({ data }) {
     <div className="space-y-8 sm:grid sm:grid-cols-2 sm:justify-items-center sm:gap-x-8 sm:gap-y-4">
       {data.length ? (
         data.map((item) => (
-          <div className="mx-auto flex w-max flex-col items-center gap-3 rounded-lg px-4 py-3 shadow-xl shadow-blue-300">
+          <div
+            key={item._id}
+            className="mx-auto flex w-max flex-col items-center gap-3 rounded-lg px-4 py-3 shadow-xl shadow-blue-300"
+          >
             <AdCard data={item} key={item._id} />
             <AdCardBt data={item} />
           </div>
